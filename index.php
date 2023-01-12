f24412777a31edcfd4cca3f1797cea6ed919442f
Dee Brecke <brecke.deirdre@student.greenriver.edu>
2023-01-12 18:14:13 -0500

    Added controller page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once("vendor/autoload.php");
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route ("home page" for hello project)
+$f3->route('GET /', function(){
+echo '<h1>Pet Home</h1>';
+//$view = new Template();
+//echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
+
+?>
